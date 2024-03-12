c5eb67f32682c3f0dc4ee0707013c30b2a24bf61
ghanam <ghanam4474@gmail.com>
2024-03-12 23:38:20 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+//app.post("/calculate", commissionCalculator.calculateSalary);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
